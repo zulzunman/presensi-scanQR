3cd6c4031d5f1d00d3b5a9961a334cef03eedd66
RidwanAgungGitHub <ridwanagung370@gmail.com>
2024-07-31 14:55:22 +0700

    css schedules

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,14 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
 <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+
 <body>
     <div class="container">
         @yield('content')
     </div>
 </body>
-    <script>
-        // @yield('scripts')
-    </script>
+<script>
+    // @yield('scripts')
+</script>
+
 </html>
--- resources/views/schedules/create.blade.php
+++ resources/views/schedules/create.blade.php
@@ -1,75 +1,60 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Create Schedule</div>
-
-                <div class="card-body">
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ url('/schedules') }}">
-                        @csrf
-
-                        <div class="form-group">
-                            <label for="subject_id">Subject</label>
-                            <select class="form-control" id="subject_id" name="subject_id" required>
-                                @foreach($subjects as $subject)
-                                    <option value="{{ $subject->id }}">{{ $subject->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="class_id">Class</label>
-                            <select class="form-control" id="class_id" name="class_id" required>
-                                @foreach($classes as $class)
-                                    <option value="{{ $class->id }}">{{ $class->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="day">Day</label>
-                            <select class="form-control" id="day" name="day" required>
-                                <option value="Monday">Monday</option>
-                                <option value="Tuesday">Tuesday</option>
-                                <option value="Wednesday">Wednesday</option>
-                                <option value="Thursday">Thursday</option>
-                                <option value="Friday">Friday</option>
-                                <option value="Saturday">Saturday</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="start_time">Start Time</label>
-                            <input type="time" class="form-control" id="start_time" name="start_time" value="{{ old('start_time') }}" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="end_time">End Time</label>
-                            <input type="time" class="form-control" id="end_time" name="end_time" value="{{ old('end_time') }}" required>
-                        </div>
-
-                        <button type="submit" class="btn btn-primary">Create Schedule</button>
-                    </form>
-
-                    <div>
-                        <a href="{{ route('schedules.index') }}">Back to Schedule</a>
+<div class="modal fade" id="createScheduleModal" tabindex="-1" role="dialog" aria-labelledby="createScheduleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="createScheduleModalLabel">Create Schedule</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="{{ route('schedules.store') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="subject_id">Subject</label>
+                        <select class="form-control" id="subject_id" name="subject_id" required>
+                            <option value="" disabled selected>Select Subject</option>
+                            @foreach ($subjects as $subject)
+                                <option value="{{ $subject->id }}">{{ $subject->name }}</option>
+                            @endforeach
+                        </select>
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label for="class_id">Class</label>
+                        <select class="form-control" id="class_id" name="class_id" required>
+                            <option value="" disabled selected>Select Class</option>
+                            @foreach ($classes as $class)
+                                <option value="{{ $class->id }}">{{ $class->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="day">Day</label>
+                        <select class="form-control" id="day" name="day" required>
+                            <option value="" disabled selected>Select Day</option>
+                            <option value="Monday">Monday</option>
+                            <option value="Tuesday">Tuesday</option>
+                            <option value="Wednesday">Wednesday</option>
+                            <option value="Thursday">Thursday</option>
+                            <option value="Friday">Friday</option>
+                            <option value="Saturday">Saturday</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="start_time">Start Time</label>
+                        <input type="time" class="form-control" id="start_time" name="start_time"
+                            value="{{ old('start_time') }}" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="end_time">End Time</label>
+                        <input type="time" class="form-control" id="end_time" name="end_time"
+                            value="{{ old('end_time') }}" required>
+                    </div>
+                    <div class="form-group d-flex justify-content-end mt-3">
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary ms-2">Add Schedule</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </div>
-@endsection
